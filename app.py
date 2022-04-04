f1c2ecfd5d5fa66bdeef005bf7ec757ac00d2fca
EpicSans002 <71330540+EpicSans002@users.noreply.github.com>
2022-04-04 16:21:50 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import amino
+import json
+import urllib.request
+from flask import Flask, jsonify, request, render_template
+import random
+import os
+
+
+
+client = amino.Client()
+def SID(self, email: str, password: str, device: str):
+   client=amino.Client(deviceId=device)
+   email=input("Email: ")
+   password=input("Password: ")
+   client.login(email,password)
+   SID = client.sid
+   return SID
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template("my-form.html")
+
+@app.route("/", methods=['POST'])
+def home_post():
+  email= request.form['email']
+  password= request.form['password']
+  device= request.form['device']
+  convert= SID(email=email, password=password, device=device”)
+  format = {"sid": f'{convert}'}
+  web = jsonify(format)
+  return web
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
